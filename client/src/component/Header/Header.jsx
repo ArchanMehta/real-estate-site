45c4d61dc8a84828dce95444cefeebc04f6cea1f
Archan Amirkumar Mehta <200020116069ait@gmail.com>
2024-05-21 15:55:51 +0530

    uri updated

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./Header.css";
 import { BiMenuAltRight } from "react-icons/bi";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
 import ProfileMenu from "../ProfileMenu/ProfileMenu.jsx";
 import AddPropertyModal from "../AddPropertyModal/AddPropertyModal.jsx";
-import useAuthCheck from "../../hooks/useAuthCheck.jsx";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const [modalOpened, setModalOpened] = useState(false);
   const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
-  const { validateLogin } = useAuthCheck();
+
+  useEffect(() => {
+    console.log("isAuthenticated:", isAuthenticated);
+    console.log("User:", user);
+  }, [isAuthenticated, user]);
 
   const getMenuStyles = (menuOpened) => {
     if (document.documentElement.clientWidth <= 800) {
@@ -24,11 +27,9 @@ const Header = () => {
     if (isAuthenticated) {
       setModalOpened(true);
     } else {
-      // Show login popup if user is not authenticated
       loginWithRedirect();
     }
   };
-
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
@@ -43,16 +44,13 @@ const Header = () => {
         >
           <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
             <NavLink to="/properties">Properties</NavLink>
-
             <a href="mailto:abc@gmail.com">Contact</a>
-
             <div onClick={handleAddPropertyClick}>
               <AddPropertyModal
                 opened={modalOpened}
                 setOpened={setModalOpened}
               />
             </div>
-
             {!isAuthenticated ? (
               <button className="button" onClick={loginWithRedirect}>
                 Login
@@ -62,7 +60,6 @@ const Header = () => {
             )}
           </div>
         </OutsideClickHandler>
-
         <div
           className="menu-icon"
           onClick={() => setMenuOpened((prev) => !prev)}
